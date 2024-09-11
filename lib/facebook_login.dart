e15b2abb2a59ae90c2601478e758755e3ae31ca6
Utsav Donda <utsav.donda@rapidops.co>
2024-09-11 11:43:49 +0530

    facebook logout

--- lib/facebook_login.dart
+++ lib/facebook_login.dart
@@ -9,6 +9,8 @@ class FacebookLogin extends StatefulWidget {
 }
 
 class _FacebookLoginState extends State<FacebookLogin> {
+  Map<String, dynamic>? _userData;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,23 +18,62 @@ class _FacebookLoginState extends State<FacebookLogin> {
         title: const Text('Facebook Login'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: facebookLogin,
-          child: const Text('Login with Facebook'),
-        ),
+        child: _userData == null
+            ? ElevatedButton(
+                onPressed: facebookLogin,
+                child: const Text('Login with Facebook'),
+              )
+            : _buildUserDetails(),
       ),
     );
   }
 
-  void facebookLogin() async {
+  Future<void> facebookLogin() async {
     final result =
         await FacebookAuth.i.login(permissions: ['public_profile', 'email']);
-    print(result);
     if (result.status == LoginStatus.success) {
       final userData = await FacebookAuth.i.getUserData();
-      print(userData);
+      setState(() {
+        _userData = userData;
+      });
     } else {
       print('Failed to login');
     }
+  }
+
+  facebookLogout() async {
+    await FacebookAuth.i.logOut();
+    setState(() {
+      _userData = null;
+    });
+  }
+
+  Widget _buildUserDetails() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _userData!['picture']['data']['is_silhouette'] == false
+            ? CircleAvatar(
+                backgroundImage:
+                    NetworkImage(_userData!['picture']['data']['url']),
+                radius: 50,
+              )
+            : const Icon(Icons.account_circle, size: 50),
+        const SizedBox(height: 10),
+        Text(
+          _userData!['name'] ?? 'No Name',
+          style: const TextStyle(fontSize: 18),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          _userData!['email'] ?? 'No Email',
+          style: const TextStyle(fontSize: 16),
+        ),
+        ElevatedButton(
+          onPressed: facebookLogout,
+          child: const Text('Logout'),
+        ),
+      ],
+    );
   }
 }
